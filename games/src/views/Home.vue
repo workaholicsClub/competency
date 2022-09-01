e15e2da61c2878dff461aa6e779a5ec8fb33def3
Alexander Pavlov <ap@tsys.pw>
2022-09-01 10:28:08 +0300

    - все игры на одной странице

--- games/src/views/Home.vue
+++ games/src/views/Home.vue
@@ -10,6 +10,11 @@
             <b-nav-item>
                 <router-link to="/shop">Заработай миллион</router-link>
             </b-nav-item>
+            <p class="px-3 py-2 m-0"><a href="/kupioxa/" target="_blank">Как Кирюха программистом стал и не зациклился</a></p>
+            <p class="px-3 py-2 m-0"><a href="/ducks/">Уточки-дифференцуточки</a></p>
+            <p class="px-3 py-2 m-0"><a href="/slope/">Лыжи едут</a></p>
+            <p class="px-3 py-2 m-0"><a href="/steepest/">Наискорейший Пью</a></p>
+            <p class="px-3 py-2 m-0"><a href="/cauldron/">100%-ный приворот</a></p>
         </b-nav>
     </b-card>
 </template>
